MiCode/Xiaomi_Kernel_OpenSource

commit 5d2a39bcac89b0b7bd5f3fac56b3f06062e9f0d7

msm: vidc: Set properties in start streaming
Cache all properties in driver and set them to firmware
in start streaming.

CRs-Fixed: 2344373
Change-Id: I5980f63a6f01260acd38bccc86574e9570b50094
Signed-off-by: Maheshwar Ajja <majja@codeaurora.org>
Signed-off-by: Vaibhav Deshu Venkatesh <vdeshuve@codeaurora.org>

drivers/media/platform/msm/vidc/msm_vidc.c
@@ -421,6 +421,24 @@ int msm_vidc_reqbufs(void *instance, struct v4l2_requestbuffers *b)
 }
 EXPORT_SYMBOL(msm_vidc_reqbufs);
 
+int msm_vidc_s_parm(void *instance, struct v4l2_streamparm *a)
+{
+	struct msm_vidc_inst *inst = instance;
+
+	if (!inst || !a) {
+		dprintk(VIDC_ERR, "%s: invalid params\n", __func__);
+		return -EINVAL;
+	}
+
+	if (is_decode_session(inst))
+		return msm_vdec_s_parm(inst, a);
+	else if (is_encode_session(inst))
+		return msm_venc_s_parm(inst, a);
+
+	return 0;
+}
+EXPORT_SYMBOL(msm_vidc_s_parm);
+
 static bool valid_v4l2_buffer(struct v4l2_buffer *b,
 		struct msm_vidc_inst *inst)
 {
@@ -765,9 +783,6 @@ static int msm_vidc_queue_setup(struct vb2_queue *q,
 			sizes[i] = inst->bufq[OUTPUT_PORT].plane_sizes[i];
 
 		bufreq->buffer_count_actual = *num_buffers;
-		rc = msm_comm_set_buffer_count(inst,
-			bufreq->buffer_count_min,
-			bufreq->buffer_count_actual, HAL_BUFFER_INPUT);
 		}
 		break;
 	case V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE: {
@@ -799,9 +814,6 @@ static int msm_vidc_queue_setup(struct vb2_queue *q,
 			sizes[i] = inst->bufq[CAPTURE_PORT].plane_sizes[i];
 
 		bufreq->buffer_count_actual = *num_buffers;
-		rc = msm_comm_set_buffer_count(inst,
-			bufreq->buffer_count_min,
-			bufreq->buffer_count_actual, buffer_type);
 		}
 		break;
 	default:
@@ -1060,6 +1072,18 @@ static int msm_vidc_set_rotation(struct msm_vidc_inst *inst)
 	return rc;
 }
 
+static int msm_vidc_set_properties(struct msm_vidc_inst *inst)
+{
+	int rc = 0;
+
+	if (is_decode_session(inst))
+		rc = msm_vdec_set_properties(inst);
+	else if (is_encode_session(inst))
+		rc = msm_venc_set_properties(inst);
+
+	return rc;
+}
+
 static inline int start_streaming(struct msm_vidc_inst *inst)
 {
 	int rc = 0;
@@ -1070,6 +1094,13 @@ static inline int start_streaming(struct msm_vidc_inst *inst)
 	dprintk(VIDC_DBG, "%s: %x : inst %pK\n", __func__,
 		hash32_ptr(inst->session), inst);
 	hdev = inst->core->device;
+
+	rc = msm_vidc_set_properties(inst);
+	if (rc) {
+		dprintk(VIDC_ERR, "%s: %x: set props failed\n",
+			__func__, hash32_ptr(inst->session));
+		goto fail_start;
+	}
 
 	if (inst->session_type == MSM_VIDC_ENCODER) {
 		rc = msm_vidc_set_rotation(inst);

drivers/media/platform/msm/vidc/hfi_packetization.c
@@ -1425,7 +1425,6 @@ int create_pkt_cmd_session_set_property(
 	case HAL_CONFIG_VENC_INTRA_PERIOD:
 	{
 		struct hfi_intra_period *hfi;
-		struct hfi_idr_period *hfi_idr_period;
 		u32 *prop_type;
 
 		pkt->rg_property_data[0] =
@@ -1447,11 +1446,24 @@ int create_pkt_cmd_session_set_property(
 			pkt->size += sizeof(u32) + sizeof(struct hfi_enable);
 			prop_type += sizeof(u32) + sizeof(struct hfi_enable);
 		}
-		*prop_type =  HFI_PROPERTY_CONFIG_VENC_IDR_PERIOD;
-		hfi_idr_period = (struct hfi_idr_period *)(prop_type + 1);
-		hfi_idr_period->idr_period = IDR_PERIOD;
-		pkt->num_properties += 1;
-		pkt->size += sizeof(struct hfi_idr_period) + sizeof(u32);
+		break;
+	}
+	case HAL_CONFIG_VENC_IDR_PERIOD:
+	{
+		struct hfi_idr_period *hfi;
+
+		pkt->rg_property_data[0] = HFI_PROPERTY_CONFIG_VENC_IDR_PERIOD;
+		hfi = (struct hfi_idr_period *) &pkt->rg_property_data[1];
+		hfi->idr_period = ((struct hfi_idr_period *) pdata)->idr_period;
+		pkt->size += sizeof(u32);
+		break;
+	}
+	case HAL_PARAM_VENC_ADAPTIVE_B:
+	{
+		create_pkt_enable(pkt->rg_property_data,
+			HFI_PROPERTY_PARAM_VENC_ADAPTIVE_B,
+			((struct hal_enable *)pdata)->enable);
+		pkt->size += sizeof(struct hfi_enable);
 		break;
 	}
 	case HAL_PARAM_VDEC_CONCEAL_COLOR:

drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
@@ -213,7 +213,7 @@ static int msm_v4l2_s_parm(struct file *file, void *fh,
 {
 	struct msm_vidc_inst *vidc_inst = get_vidc_inst(file, fh);
 
-	return msm_vidc_comm_s_parm(vidc_inst, a);
+	return msm_vidc_s_parm(vidc_inst, a);
 }
 
 static int msm_v4l2_g_parm(struct file *file, void *fh,

drivers/media/platform/msm/vidc/msm_vdec.h
@@ -12,8 +12,15 @@
 int msm_vdec_inst_init(struct msm_vidc_inst *inst);
 int msm_vdec_ctrl_init(struct msm_vidc_inst *inst,
 	const struct v4l2_ctrl_ops *ctrl_ops);
-int msm_vdec_enum_fmt(void *instance, struct v4l2_fmtdesc *f);
-int msm_vdec_s_fmt(void *instance, struct v4l2_format *f);
-int msm_vdec_s_ctrl(void *instance, struct v4l2_ctrl *ctrl);
-int msm_vdec_g_ctrl(void *instance, struct v4l2_ctrl *ctrl);
+int msm_vdec_enum_fmt(struct msm_vidc_inst *inst,
+		struct v4l2_fmtdesc *f);
+int msm_vdec_s_fmt(struct msm_vidc_inst *inst,
+		struct v4l2_format *f);
+int msm_vdec_s_ctrl(struct msm_vidc_inst *inst,
+		struct v4l2_ctrl *ctrl);
+int msm_vdec_g_ctrl(struct msm_vidc_inst *inst,
+		struct v4l2_ctrl *ctrl);
+int msm_vdec_s_parm(struct msm_vidc_inst *inst,
+		struct v4l2_streamparm *a);
+int msm_vdec_set_properties(struct msm_vidc_inst *inst);
 #endif

drivers/media/platform/msm/vidc/msm_venc.h
@@ -11,9 +11,25 @@
 
 int msm_venc_inst_init(struct msm_vidc_inst *inst);
 int msm_venc_ctrl_init(struct msm_vidc_inst *inst,
-	const struct v4l2_ctrl_ops *ctrl_ops);
-int msm_venc_enum_fmt(void *instance, struct v4l2_fmtdesc *f);
-int msm_venc_s_fmt(void *instance, struct v4l2_format *f);
-int msm_venc_s_ctrl(void *instance, struct v4l2_ctrl *ctrl);
-
+		const struct v4l2_ctrl_ops *ctrl_ops);
+int msm_venc_enum_fmt(struct msm_vidc_inst *inst,
+		struct v4l2_fmtdesc *f);
+int msm_venc_s_fmt(struct msm_vidc_inst *inst,
+		struct v4l2_format *f);
+int msm_venc_s_ctrl(struct msm_vidc_inst *inst,
+		struct v4l2_ctrl *ctrl);
+int msm_venc_s_parm(struct msm_vidc_inst *inst,
+		struct v4l2_streamparm *a);
+int msm_venc_set_properties(struct msm_vidc_inst *inst);
+int msm_venc_set_extradata(struct msm_vidc_inst *inst);
+int msm_venc_set_frame_rate(struct msm_vidc_inst *inst);
+int msm_venc_set_bitrate(struct msm_vidc_inst *inst);
+int msm_venc_set_operating_rate(struct msm_vidc_inst *inst);
+int msm_venc_set_idr_period(struct msm_vidc_inst *inst);
+int msm_venc_set_intra_period(struct msm_vidc_inst *inst);
+int msm_venc_set_ltr_useframe(struct msm_vidc_inst *inst);
+int msm_venc_set_ltr_markframe(struct msm_vidc_inst *inst);
+int msm_venc_set_dyn_qp(struct msm_vidc_inst *inst);
+int msm_venc_set_request_keyframe(struct msm_vidc_inst *inst);
+int msm_venc_set_intra_refresh_mode(struct msm_vidc_inst *inst);
 #endif

drivers/media/platform/msm/vidc/msm_vidc.h
@@ -113,6 +113,7 @@ int msm_vidc_s_ext_ctrl(void *instance, struct v4l2_ext_controls *a);
 int msm_vidc_g_ext_ctrl(void *instance, struct v4l2_ext_controls *a);
 int msm_vidc_g_ctrl(void *instance, struct v4l2_control *a);
 int msm_vidc_reqbufs(void *instance, struct v4l2_requestbuffers *b);
+int msm_vidc_s_parm(void *instance, struct v4l2_streamparm *a);
 int msm_vidc_release_buffer(void *instance, int buffer_type,
 		unsigned int buffer_index);
 int msm_vidc_qbuf(void *instance, struct v4l2_buffer *b);

amiami5/android_kernel_xiaomi_sm8250

commit 26d4e8c33434808e029226aad98b7dc6e88633cf

msm: vidc: Fix compiler warnings
Fix a handful of compiler warnings; mostly printk format errors but a
few unused variables as well.

Fixes: 5d2a39bcac89 ("msm: vidc: Set properties in start streaming")
Fixes: e8507af28b12 ("msm: vidc: Add snapshot of MSM video driver")
Change-Id: Ic0dedbad8d3a7f85fc9e117547cd239a987c3693
Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

drivers/media/platform/msm/vidc/hfi_response_handler.c
@@ -1829,7 +1829,7 @@ static int hfi_process_sys_property_info(u32 device_id,
 	default:
 		dprintk(VIDC_DBG,
 				"%s: unknown_prop_id: %x\n",
-				pkt->rg_property_data[0], __func__);
+				__func__, pkt->rg_property_data[0]);
 		return -ENOTSUPP;
 	}
 

drivers/media/platform/msm/vidc/msm_smem.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  */
 
 #include <asm/dma-iommu.h>
@@ -520,7 +520,8 @@ int msm_smem_cache_operations(struct dma_buf *dbuf,
 	/* Return if buffer doesn't support caching */
 	rc = dma_buf_get_flags(dbuf, &flags);
 	if (rc) {
-		dprintk(VIDC_ERR, "%s: dma_buf_get_flags failed, err %d\n", rc);
+		dprintk(VIDC_ERR, "%s: dma_buf_get_flags failed, err %d\n",
+			__func__, rc);
 		return rc;
 	} else if (!(flags & ION_FLAG_CACHED)) {
 		return rc;

drivers/media/platform/msm/vidc/msm_vdec.c
@@ -1400,7 +1400,7 @@ int msm_vdec_set_output_stream_mode(struct msm_vidc_inst *inst)
 		if (rc) {
 			dprintk(VIDC_ERR,
 				"%s: set prop frame_size failed\n",
-				__func__, rc);
+				__func__);
 			return rc;
 		}
 	}

drivers/media/platform/msm/vidc/msm_venc.c
@@ -1953,7 +1953,7 @@ int msm_venc_set_secure_mode(struct msm_vidc_inst *inst)
 	ctrl = get_ctrl(inst, V4L2_CID_MPEG_VIDC_VIDEO_SECURE);
 	enable.enable = !!ctrl->val;
 
-	dprintk(VIDC_DBG, "%s: %d\n", __func__, enable);
+	dprintk(VIDC_DBG, "%s: %d\n", __func__, enable.enable);
 	rc = call_hfi_op(hdev, session_set_property, inst->session,
 		HFI_PROPERTY_PARAM_SECURE_SESSION, &enable, sizeof(enable));
 	if (rc)
@@ -1978,7 +1978,7 @@ int msm_venc_set_priority(struct msm_vidc_inst *inst)
 	ctrl = get_ctrl(inst, V4L2_CID_MPEG_VIDC_VIDEO_PRIORITY);
 	enable.enable = !!ctrl->val;
 
-	dprintk(VIDC_DBG, "%s: %d\n", __func__, enable);
+	dprintk(VIDC_DBG, "%s: %d\n", __func__, enable.enable);
 	rc = call_hfi_op(hdev, session_set_property, inst->session,
 		HFI_PROPERTY_CONFIG_REALTIME, &enable, sizeof(enable));
 	if (rc)
@@ -3040,7 +3040,7 @@ int msm_venc_set_ltr_mode(struct msm_vidc_inst *inst)
 		return 0;
 	if (ctrl->val > inst->capability.ltr_count.max) {
 		dprintk(VIDC_ERR, "%s: invalid ltr count %d, max %d\n",
-			__func__, ctrl->val > inst->capability.ltr_count.max);
+			__func__, ctrl->val, inst->capability.ltr_count.max);
 		return -EINVAL;
 	}
 	ltr.ltr_count =  ctrl->val;

drivers/media/platform/msm/vidc/msm_vidc.c
@@ -963,7 +963,7 @@ static inline int start_streaming(struct msm_vidc_inst *inst)
 		rc = msm_vidc_set_rotation(inst);
 		if (rc) {
 			dprintk(VIDC_ERR,
-				"Set rotation for encoder failed %pK\n");
+				"Set rotation for encoder failed %pK\n", inst);
 			goto fail_start;
 		}
 	} else if ((inst->session_type == MSM_VIDC_DECODER) &&

drivers/media/platform/msm/vidc/msm_vidc_clocks.c
@@ -43,7 +43,7 @@ struct msm_vidc_core_ops core_ops_iris2 = {
 static inline void msm_dcvs_print_dcvs_stats(struct clock_data *dcvs)
 {
 	dprintk(VIDC_PROF,
-		"DCVS: Load_Low %d, Load Norm %d, Load High %d\n",
+		"DCVS: Load_Low %lld, Load Norm %lld, Load High %lld\n",
 		dcvs->load_low,
 		dcvs->load_norm,
 		dcvs->load_high);
@@ -612,7 +612,7 @@ static unsigned long msm_vidc_calc_freq_ar50(struct msm_vidc_inst *inst,
 
 	msm_dcvs_print_dcvs_stats(dcvs);
 
-	dprintk(VIDC_PROF, "%s Inst %pK : Filled Len = %d Freq = %lu\n",
+	dprintk(VIDC_PROF, "%s Inst %pK : Filled Len = %d Freq = %llu\n",
 		__func__, inst, filled_len, freq);
 
 	return (unsigned long) freq;
@@ -704,7 +704,7 @@ static unsigned long msm_vidc_calc_freq_iris1(struct msm_vidc_inst *inst,
 		dcvs->load_norm;
 
 	dprintk(VIDC_PROF,
-		"%s: inst %pK: %x : filled len %d required freq %lu load_norm %lu\n",
+		"%s: inst %pK: %x : filled len %d required freq %llu load_norm %llu\n",
 		__func__, inst, hash32_ptr(inst->session),
 		filled_len, freq, dcvs->load_norm);
 
@@ -797,7 +797,7 @@ static unsigned long msm_vidc_calc_freq_iris2(struct msm_vidc_inst *inst,
 		dcvs->load_norm;
 
 	dprintk(VIDC_PROF,
-		"%s: inst %pK: %x : filled len %d required freq %lu load_norm %lu\n",
+		"%s: inst %pK: %x : filled len %d required freq %llu load_norm %llu\n",
 		__func__, inst, hash32_ptr(inst->session),
 		filled_len, freq, dcvs->load_norm);
 

drivers/media/platform/msm/vidc/msm_vidc_dyn_gov.c
@@ -600,9 +600,9 @@ static unsigned long __calculate_encoder(struct vidc_bus_vote_data *d,
 	/* Encoder Parameters */
 	int width, height, fps, lcu_size, bitrate, lcu_per_frame,
 		collocated_bytes_per_lcu, tnbr_per_lcu, dpb_bpp,
-		original_color_format, vertical_tile_width;
+		original_color_format, vertical_tile_width, rotation;
 	bool work_mode_1, original_compression_enabled,
-		low_power, rotation, cropping_or_scaling,
+		low_power, cropping_or_scaling,
 		b_frames_enabled = false,
 		llc_ref_chroma_cache_enabled = false,
 		llc_top_line_buf_enabled = false,

drivers/media/platform/msm/vidc/venus_hfi.c
@@ -282,11 +282,11 @@ static int __dsp_send_hfi_queue(struct venus_hfi_device *device)
 	}
 
 	if (device->dsp_flags & DSP_INIT) {
-		dprintk(VIDC_DBG, "%s: dsp already inited\n");
+		dprintk(VIDC_DBG, "%s: dsp already inited\n", __func__);
 		return 0;
 	}
 
-	dprintk(VIDC_DBG, "%s: hfi queue %#x size %d\n",
+	dprintk(VIDC_DBG, "%s: hfi queue %#llx size %d\n",
 		__func__, device->dsp_iface_q_table.mem_data.dma_handle,
 		device->dsp_iface_q_table.mem_data.size);
 	rc = fastcvpd_video_send_cmd_hfi_queue(
@@ -1661,7 +1661,7 @@ static int __interface_dsp_queues_init(struct venus_hfi_device *dev)
 		goto fail_dma_map;
 	}
 	dprintk(VIDC_DBG,
-		"%s: kvaddr %pK dma_handle %#lx iova %#lx size %d\n",
+		"%s: kvaddr %pK dma_handle %#llx iova %#llx size %zd\n",
 		__func__, kvaddr, dma_handle, iova, q_size);
 
 	memset(mem_data, 0, sizeof(struct msm_smem));