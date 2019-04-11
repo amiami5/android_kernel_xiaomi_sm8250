MotorolaMobilityLLC/kernel-msm

cnss2: Add time clock frequency for target cap message
Firmware sends device time clock frequency to host through target
capability QMI message so that host is able to use it to calculate
device timestamp properly.

Change-Id: I77b348b054ba21a6a6f4564dbbd60588f6adaefe
Signed-off-by: Yue Ma <yuem@codeaurora.org>

drivers/net/wireless/cnss2/wlan_firmware_service_v01.c
@@ -1249,6 +1249,24 @@ struct qmi_elem_info wlfw_cap_resp_msg_v01_ei[] = {
 					   voltage_mv),
 	},
 	{
+		.data_type      = QMI_OPT_FLAG,
+		.elem_len       = 1,
+		.elem_size      = sizeof(u8),
+		.is_array       = NO_ARRAY,
+		.tlv_type       = 0x17,
+		.offset         = offsetof(struct wlfw_cap_resp_msg_v01,
+					   time_freq_hz_valid),
+	},
+	{
+		.data_type      = QMI_UNSIGNED_4_BYTE,
+		.elem_len       = 1,
+		.elem_size      = sizeof(u32),
+		.is_array       = NO_ARRAY,
+		.tlv_type       = 0x17,
+		.offset         = offsetof(struct wlfw_cap_resp_msg_v01,
+					   time_freq_hz),
+	},
+	{
 		.data_type      = QMI_EOTI,
 		.is_array       = NO_ARRAY,
 		.tlv_type       = QMI_COMMON_TLV_TYPE,

amiami5/android_kernel_xiaomi_sm8250

drivers/net/wireless/cnss2/wlan_firmware_service_v01.c
@@ -1239,6 +1239,24 @@ struct qmi_elem_info wlfw_cap_resp_msg_v01_ei[] = {
 					   voltage_mv),
 	},
 	{
+		.data_type      = QMI_OPT_FLAG,
+		.elem_len       = 1,
+		.elem_size      = sizeof(u8),
+		.array_type       = NO_ARRAY,
+		.tlv_type       = 0x17,
+		.offset         = offsetof(struct wlfw_cap_resp_msg_v01,
+					   time_freq_hz_valid),
+	},
+	{
+		.data_type      = QMI_UNSIGNED_4_BYTE,
+		.elem_len       = 1,
+		.elem_size      = sizeof(u32),
+		.array_type       = NO_ARRAY,
+		.tlv_type       = 0x17,
+		.offset         = offsetof(struct wlfw_cap_resp_msg_v01,
+					   time_freq_hz),
+	},
+	{
 		.data_type      = QMI_EOTI,
 		.array_type       = NO_ARRAY,
 		.tlv_type       = QMI_COMMON_TLV_TYPE,